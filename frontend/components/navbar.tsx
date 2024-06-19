eb40cf62e05246801149159aaba674d1eff4e06a
kevin hu <kevinhu@kevins-MacBook-Pro.local>
2024-06-20 09:04:26 +1000

    go back to dahsboard

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -131,7 +131,7 @@ export const Navbar = () => {
                                       d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                             </svg>
                         </button>
-                        <a href="/dashboard" className="flex ms-2 md:me-24">
+                        <a href="/" className="flex ms-2 md:me-24">
                             {/*<img src="/logo.png" className="h-8 me-3"*/}
                             {/*     alt="FlowBite Logo"/>*/}
                             <span
